f0e04e15dd154fdce69ef59eff48019bb7215d0f
unenglishable <unenglishable@gmail.com>
2014-10-02 17:49:18 -1000

    performance tweaks on labeler
    use move instead of down() right()
    get rid of clearing

--- labeler.js
+++ labeler.js
@@ -8,18 +8,15 @@ module.exports = function(label) {
   labeler.number = labelerCount;
   labelerCount++;
   labeler.write = function(string) {
-    cursor.down(this.number);
-    cursor.right(this.label.length);
-    cursor.clearToEOL();
-    process.stdout.write(chalk.yellow.bold(string));
-    cursor.left(this.label.length+string.length);
+    cursor.move(this.label.length, this.number);
+    process.stdout.write(chalk.green.bold(string)+'\r');
     cursor.up(this.number);
   };
   labeler.init = function() {
     cursor.down(this.number);
     process.stdout.write('\n');
     cursor.up();
-    process.stdout.write(chalk.green.bold(this.label));
+    process.stdout.write(chalk.blue.bold(this.label));
     process.stdout.write('\r');
     cursor.up(this.number);
   };
