13ce3a37b20a4d54f4407eac19fe00a70b7f5c13
unenglishable <unenglishable@gmail.com>
2014-10-03 12:51:33 -1000

    enhance performance

--- labeler.js
+++ labeler.js
@@ -1,24 +1,25 @@
 var cursor = require('cursed');
 var chalk = require('chalk');
 var labelerCount = 0;
+var currentRow = 0;
 
 module.exports = function(label) {
   var labeler = {};
   labeler.label = label;
-  labeler.number = labelerCount;
+  labeler.row = labelerCount;
   labelerCount++;
   labeler.write = function(string) {
-    cursor.move(this.label.length, this.number);
+    cursor.move(this.label.length, this.row - currentRow);
     process.stdout.write(chalk.green.bold(string)+'\r');
-    cursor.up(this.number);
+    currentRow = this.row;
   };
   labeler.init = function() {
-    cursor.down(this.number);
+    cursor.down(this.row - currentRow);
     process.stdout.write('\n');
     cursor.up();
     process.stdout.write(chalk.blue.bold(this.label));
     process.stdout.write('\r');
-    cursor.up(this.number);
+    cursor.up(this.row - currentRow);
   };
   labeler.init();
   return labeler;
