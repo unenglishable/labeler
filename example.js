5154f6b09f53b550d30742669894fadce1097f76
unenglishable <unenglishable@gmail.com>
2014-10-07 13:49:10 -1000

    rewrite example to use new implementation

--- example.js
+++ example.js
@@ -1,27 +1,27 @@
-var labeler = require('./labeler');
+var path = require('path');
+var labeler = require(path.join(__dirname, 'index'));
 var threshold = 25;
 
 labelers = [
-  labeler('first:  '),
-  labeler('second: '),
-  labeler('third:  '),
-  labeler('fourth:  '),
-  labeler('fifth:  '),
-  labeler('sixth:  '),
-  labeler('seventh:  '),
-  labeler('eighth:  '),
-  labeler('ninth:  '),
-  labeler('tenth:  ')
+  {labeler: labeler('first:  '), value: 0},
+  {labeler: labeler('second: '), value: 0},
+  {labeler: labeler('third:  '), value: 0},
+  {labeler: labeler('fourth:  '), value: 0},
+  {labeler: labeler('fifth:  '), value: 0},
+  {labeler: labeler('sixth:  '), value: 0},
+  {labeler: labeler('seventh:  '), value: 0},
+  {labeler: labeler('eighth:  '), value: 0},
+  {labeler: labeler('ninth:  '), value: 0},
+  {labeler: labeler('tenth:  '), value: 0}
 ];
 
-var loop = function(labeler, wait) {
+var loop = function(labelPair, wait) {
   setInterval(function() {
-    labeler.write(labeler.value.toString());
-    labeler.value++;
+    labelPair.labeler.write(labelPair.value.toString());
+    labelPair.value++;
   }, wait);
 };
 
-labelers.forEach(function(labeler) {
-  labeler.value = 0;
-  loop(labeler, Math.floor((Math.random() * threshold) + 1));
+labelers.forEach(function(labelPair) {
+  loop(labelPair, Math.floor((Math.random() * threshold) + 1));
 });
