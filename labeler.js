52eff4fb4051c7da77ac59f094b127c2af3d767f
unenglishable <unenglishable@gmail.com>
2014-10-07 13:48:10 -1000

    use object/prototype model for labeler

--- labeler.js
+++ labeler.js
@@ -1,26 +1,21 @@
 var cursor = require('cursed');
 var chalk = require('chalk');
-var labelerCount = 0;
 var currentRow = 0;
 
-module.exports = function(label) {
-  var labeler = {};
-  labeler.label = label;
-  labeler.row = labelerCount;
-  labelerCount++;
-  labeler.write = function(string) {
-    cursor.move(this.label.length, this.row - currentRow);
-    process.stdout.write(chalk.green.bold(string)+'\r');
-    currentRow = this.row;
-  };
-  labeler.init = function() {
-    cursor.down(this.row - currentRow);
-    process.stdout.write('\n');
-    cursor.up();
-    process.stdout.write(chalk.blue.bold(this.label));
-    process.stdout.write('\r');
-    cursor.up(this.row - currentRow);
-  };
-  labeler.init();
-  return labeler;
+var Labeler = module.exports = function(options) {
+  this.label = options.label;
+  this.row = options.row;
+};
+Labeler.prototype.write = function(string) {
+  cursor.move(this.label.length, this.row - currentRow);
+  process.stdout.write(chalk.green.bold(string)+'\r');
+  currentRow = this.row;
+};
+Labeler.prototype.init = function() {
+  cursor.down(this.row - currentRow);
+  process.stdout.write('\n');
+  cursor.up();
+  process.stdout.write(chalk.blue.bold(this.label));
+  process.stdout.write('\r');
+  cursor.up(this.row - currentRow);
 };
